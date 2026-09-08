d7118f1aa8d3bbd2a8458921968e7eb6c36eb84d
eslam <eslamayman2372002ii@gamail.com>
2026-09-08 16:26:13 +0300

    chapter 01 challenge 01 , 02 , 03 done

---
+++ chapter_01/ch_03/index.ts
@@ -0,0 +1,7 @@
+function reportErrors(username: string, age: number) {
+    let rank = "Professor";
+    return `Username: ${username}, age: ${age}, jop: ${rank}`;
+    // console.log("We Will Not Reach Here");
+}
+
+console.log(reportErrors("El zero", 40));
---
+++ chapter_01/ch_04/index.ts
@@ -0,0 +1,9 @@
+let nothing;
+let theName: string = "Elzero";
+
+function showMyDetails(a = "", b = "", c?: any) {
+    return `${a}${b}${c}`;
+}
+
+// Replace ???? With The Available Variables As Argument To Get The Result
+console.log(showMyDetails(theName)); // Elzero
